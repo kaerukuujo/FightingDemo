d3b8da1f31ec9310286c1c672cd47b78e593be54
kaerukuujo <mrjolyne@gmail.com>
2023-01-08 13:32:30 +0000

    working healthbars  + damage dealing

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ class Sprite {
         };
         this.color = color; 
         this.isAttacking;
+        this.health = 100;
     }
 
     draw() {
@@ -161,6 +162,8 @@ function animate() {
         player.isAttacking
     ) {
         player.isAttacking = false;
+        enemy.health -= 20;
+        document.querySelector('#enemyHealth').style.width = enemy.health + '%';
         console.log('player attack hit');
     }
 
@@ -172,6 +175,8 @@ function animate() {
         enemy.isAttacking
     ) {
         enemy.isAttacking = false;
+        player.health -= 20;
+        document.querySelector('#playerHealth').style.width = player.health + '%';
         console.log('enemy attack hit');
     }
 
